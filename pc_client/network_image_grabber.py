a745e3eb5be037fc38c321bcbf9e00f85180dba5
Tobias Beckmann <tobias.beckmann@ipm.fraunhofer.de>
2018-04-27 13:31:27 +0200

    Add server (rpi) for mjpeg and bayer images Add client (pc) for testing (parts of) the server.

---
+++ pc_client/network_image_grabber.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import urllib.request
+import xmlrpc.client
+from PyQt5.QtCore import *
+import numpy
+import time
+import sys
+import socket
+from PIL import Image
+import io
+
+
+class RemoteImageGrabber(QThread):
+
+    refresh_3d_sig = pyqtSignal(object, object)
+    refresh_preview = pyqtSignal(object)
+
+    def __init__(self, single_image_loc, remote_server):
+        self.server = xmlrpc.client.ServerProxy('http://'+remote_server+':5117/RPC2')
+        start = False
+        try:
+            start = self.server.is_online()
+        except socket.error:
+            print("Error: Could not connect to  "+remote_server+". Can you ping the rpi?")
+        except:
+            print("Error connecting - is the server script running on the rpi?")
+
+        self.single_image = single_image_loc
+        self.remote_server = remote_server
+        self.stopping = False
+        if start:
+            QThread.__init__(self)
+        else:
+            sys.exit()
+
+    # This method is called after the thread has been started
+    def run(self):
+        if self.single_image:
+            print("new RemoteImageGrabber starting in single_image mode...")
+        else:
+            self.fetch_images()
+
+    def stop(self):
+        self.stopping = True
+
+    def fetch_images(self):
+        """ get images from the mjpeg_streamer"""
+        mjpeg_available = self.server.activate_stream(True)
+        while not self.stopping:
+            if mjpeg_available:
+                stream = urllib.request.urlopen('http://'+self.remote_server+':8080/?action=stream')
+                byte_array = b''
+                while True:
+                    byte_array += stream.read(1024)
+                    a = byte_array.find(b'\xff\xd8')
+                    b = byte_array.find(b'\xff\xd9')
+                    if a != -1 and b != -1:
+                        jpg = byte_array[a:b + 2]
+                        byte_array = byte_array[b + 2:]
+                        byte_stream = io.BytesIO(jpg)
+                        i = numpy.array(Image.open(byte_stream))[:, :, 0]
+                        self.refresh_preview.emit(i)
+                        if self.stopping:
+                            stream.close()
+                            self.server.deactivate_stream()
+                            break
+            else:
+                while True:
+                    start = time.time()
+                    jpeg_tuple = self.server.take_single_jpeg()
+                    array = numpy.fromstring(str(jpeg_tuple), dtype=numpy.uint8)
+                    w = 1024
+                    h = 1024
+                    image = numpy.multiply(array[:w * h].reshape((h, w)),2, dtype=numpy.uint8)
+                    self.refresh_preview.emit(image)
+                    print(time.time()-start)
+                    if self.stopping:
+                        break
+
+    def order_single_bayer_image(self, shape):
+        """Get 16-bit uncompressed image from rpi"""
+        print("getting bayer image...")
+        t0 = time.time()
+        image_string = self.server.take_single_bayer(*shape).data
+        t1 = time.time()
+        print("Bayer image transferred in {:.1} s".format(t1-t0))
+
+        image = numpy.fromstring(image_string, numpy.uint16)
+        assert(image.size == shape[0]*shape[1])
+
+        self.refresh_3d_sig.emit(image.reshape(shape), True)
+        return image.reshape(shape)
+
+    def order_random_image(self, highest, shape):
+        """Test 16-bit transmission without needing a camera: get random data from rpi"""
+        image_string = self.server.take_random_16bit(highest, shape)
+        image = numpy.fromstring(image_string.data, dtype=numpy.uint16)
+        return image.reshape(shape)
+
+if __name__ == '__main__':
+    import pylab
+    server_addres = '10.82.202.30'
+    request_shape = (128, 128)
+    highest_in_16_bit = 1000
+
+    Grabber = RemoteImageGrabber(False, server_addres)
+    # Test with random data
+    random_image = Grabber.order_random_image(highest_in_16_bit, request_shape)
+    print("Random sata:", random_image.shape, "max:", numpy.max(random_image))
+
+    # Test using Picamera
+    cam_image_bayer = Grabber.order_single_bayer_image(request_shape)
+    print("Camera image:", cam_image_bayer.shape, "max:", numpy.max(cam_image_bayer))
+
+    pylab.imshow(cam_image_bayer, interpolation="None", cmap="gray")
+    pylab.show()
---
+++ rpi_server/rpi_server.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+import subprocess
+import time
+import numpy as np
+from xmlrpc.server import SimpleXMLRPCServer
+from xmlrpc.server import SimpleXMLRPCRequestHandler
+
+try:
+    import picamera
+    import picamera.array
+except ImportError:
+    print("Kamera konnte nicht initalisiert werden. Beende Programm!.")
+    exit()
+
+
+# --------------------Variables--------------------------
+w_full, h_full = 3280, 2464  # full resolution - needed for raw (PiBayerArray) mode 3280, 2464
+shutter_speed = 60
+use_stream = True
+proc = None
+
+
+class CameraArbiter:
+    def __init__(self):
+        self.streamActive = False
+        self.pythonActive = False
+        self.proc = None
+        self.camera = None
+
+    def activate_stream(self):
+        if self.pythonActive:
+            self.pythonActive = False
+            self.camera.close()
+            self.camera = None
+        if self.proc is not None:
+            self.proc.terminate()
+        self.streamActive = True
+        self.proc = subprocess.Popen(['/usr/src/mjpg-streamer/mjpg-streamer-experimental/mjpg_streamer',
+                                     '-o',
+                                     '/usr/src/mjpg-streamer/mjpg-streamer-experimental/output_http.so -w /usr/src/mjpg-streamer/mjpg-streamer-experimental/www',
+                                     '-i',
+                                     '/usr/src/mjpg-streamer/mjpg-streamer-experimental/input_raspicam.so -x 1024 -y 1024 -fps 15 -ex auto -quality 99'])
+        time.sleep(1.5)  # TODO: find better fix for the stream warm up problem
+
+    def deactivate_stream(self):
+        if self.streamActive:
+            self.proc.terminate()
+            self.streamActive = False
+
+    def claim_cam(self):
+        if self.streamActive:
+            self.proc.terminate()
+            self.streamActive = False
+        if self.camera is None:
+            print("create PiCam")
+            self.pythonActive = True
+            self.camera = picamera.PiCamera()
+            print("Created, waiting.")
+            time.sleep(1)
+        return self.camera
+
+
+def activate_stream(mjpeg):
+    if use_stream and mjpeg:
+        arbiter.activate_stream()
+        return True
+    else:
+        print("Ask for the images on your own!")
+        return False
+
+
+def deactivate_stream():
+    print("Stream deactivated")
+    arbiter.deactivate_stream()
+    return True
+
+
+def take_single_bayer(w_out, h_out):
+    print("got bayer request")
+    # arbiter.claim_cam().shutter_speed = shutter_speed  # Auto-Belichtung geht ganz gut.
+    arbiter.claim_cam().resolution = (w_full, h_full)
+    output = picamera.array.PiBayerArray(arbiter.claim_cam())
+    output.truncate(0)
+    arbiter.claim_cam().capture(output, 'jpeg', bayer=True)
+
+    im = output.array[1::2, 1::2, 0]  # red, one out of each 2x2
+
+    h_0 = (h_full/2 - h_out) / 2
+    w_0 = (w_full/2 - w_out) / 2
+
+    im = im[h_0:h_0 + h_out, w_0:w_0 + w_out]
+    print("output: {}x{}, [{}...{}], type {}".format(im.shape[0], im.shape[1], np.min(im), np.max(im), im.dtype))
+
+    return im.astype(np.uint16).tostring()
+
+
+def take_single_jpeg():
+    # Camera in Global and close when bayer is needed
+    image = np.empty((1024, 1024, 3), dtype=np.uint8)
+    arbiter.claim_cam().shutter_speed = shutter_speed
+    arbiter.claim_cam().resolution = (1024, 1024)
+    arbiter.claim_cam().capture(image, 'rgb')
+    im = image[:, :, 0]
+    im = np.divide(im, 2)
+    return im.astype(dtype=np.uint8).tostring()
+
+
+def take_random_16bit(highest, shape):
+    array = np.random.randint(highest, size=shape)
+    return array.astype(np.uint16).tostring()
+
+
+def is_online():
+    return True
+
+
+# Create server
+class RequestHandler(SimpleXMLRPCRequestHandler):
+    rpc_paths = ('/RPC2',)
+
+server = SimpleXMLRPCServer(("", 5117), requestHandler=RequestHandler)
+server.register_function(take_single_bayer, 'take_single_bayer')
+server.register_function(take_single_jpeg, 'take_single_jpeg')
+server.register_function(take_random_16bit, 'take_random_16bit')
+server.register_function(activate_stream, 'activate_stream')
+server.register_function(deactivate_stream, 'deactivate_stream')
+server.register_function(is_online, 'is_online')
+# Um die Ip des Servers herauszufinden, folgenden Befehl im Linux terminal ausführen:
+# ifconfig eth0 | grep 'inet Adresse'
+print("Server up!")
+
+arbiter = CameraArbiter()
+
+server.serve_forever()
